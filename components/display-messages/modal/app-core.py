c169245e1a5646ed98288af626dcd9c30944d371
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-09 12:28:08 -0500

    expressify components/display-messages/modal

--- components/display-messages/modal/app-core.py
+++ components/display-messages/modal/app-core.py
@@ -1,12 +1,12 @@
 from shiny import App, reactive, ui
 
-app_ui = ui.page_fluid(
+app_ui = ui.page_fixed(
     ui.input_action_button("show", "Show modal dialog"),
 )
 
 
 def server(input, output, session):
-    @reactive.Effect
+    @reactive.effect
     @reactive.event(input.show)
     def _():
         m = ui.modal(  # <<
--- components/display-messages/modal/app-variation-modal-express.py
+++ components/display-messages/modal/app-variation-modal-express.py
@@ -1,29 +1,25 @@
-# FIXME: Rewrite to Express
-from shiny import App, reactive, ui
+from shiny import reactive
+from shiny.express import input, ui
 
-app_ui = ui.page_fluid(
-    ui.input_action_button("login", "Login"),
-)
+ui.input_action_button("login", "Login to database")
 
 
-def server(input, output, session):
-    @reactive.Effect
-    @reactive.event(input.login)
-    def _():
-        m = ui.modal(
-            ui.input_text("name", "Username:"),  #<<
-            ui.input_password("password", "Password:"),  #<<
-            ui.input_action_button("connect", "Connect"),  #<<
-            easy_close=True,  #<<
-            footer=None,  #<<
-        )
-        ui.modal_show(m)
-
-    @reactive.Effect  #<<
-    @reactive.event(input.connect)  #<<
-    def __():  #<<
-        ui.modal_remove()  #<<
-        # Code to connect with input.name() and input.password() #<<
+@reactive.effect
+@reactive.event(input.login)
+def show_login_modal():
+    m = ui.modal(  # <<
+        ui.input_text("name", "Username:"),
+        ui.input_password("password", "Password:"),
+        ui.input_action_button("connect", "Connect"),
+        title="Database Credentials",  # <<
+        easy_close=True,  # <<
+        footer=None,  # <<
+    )  # <<
+    ui.modal_show(m)
 
 
-app = App(app_ui, server)
+@reactive.effect  # <<
+@reactive.event(input.connect)  # <<
+def connect_to_databaset():  # <<
+    ui.modal_remove()  # <<
+    # Code to connect with input.name() and input.password() #<<
