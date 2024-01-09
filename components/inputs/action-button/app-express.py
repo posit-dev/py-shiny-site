74e558a0002d2f3411a6a30a170eb2cccfe29192
Karan Gathani <karan.gathani@posit.co>
2024-01-09 11:07:39 -0800

    Update action button

--- components/inputs/action-button/app-express.py
+++ components/inputs/action-button/app-express.py
@@ -1,15 +1,13 @@
-# FIXME: Rewrite as an Express app
-from shiny import App, reactive, render, ui
+from shiny import reactive, render
+from shiny.express import input, ui
 
-app_ui = ui.page_fluid(
-  ui.input_action_button("action_button", "Action"), #<<
-  ui.output_text("counter")
-)
 
-def server(input, output, session):
-  @render.text()
-  @reactive.event(input.action_button)
-  def counter():
-      return f"{input.action_button()}"
+ui.page_opts(full_width=True)
 
-app = App(app_ui, server)
+ui.input_action_button("action_button", "Action")
+
+
+@render.text()
+@reactive.event(input.action_button)
+def counter():
+    return f"{input.action_button()}"
