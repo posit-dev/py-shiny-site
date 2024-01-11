8c0353b3b6ca2a6a4a18a798325ffc3a0d466552
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-11 12:37:28 -0500

    expressify components/outputs/image

--- components/outputs/image/app-core.py
+++ components/outputs/image/app-core.py
@@ -1,17 +1,20 @@
-from shiny import App, render, ui
-from shiny.types import ImgData
 from pathlib import Path
+
+from shiny import App, render, ui
+
 here = Path(__file__).parent
 
 app_ui = ui.page_fluid(
-    ui.input_checkbox("show", "Show image?", value = True),
-    ui.output_image("image") #<<
+    ui.input_checkbox("show", "Show image?", value=True),
+    ui.output_image("image"),  # <<
 )
 
+
 def server(input, output, session):
-    @render.image #<<
+    @render.image  # <<
     def image():
-        img = {"src": here/"shiny.png", "width": "100px"} #<<
+        img = {"src": here / "shiny.png", "width": "100px"}  # <<
         return img if input.show() else None
+
 
 app = App(app_ui, server)
