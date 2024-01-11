26c8d2d9cc2812ac9f1472b702867b2b47de8dfb
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-11 15:26:13 -0500

    expressify components/outputs/plot-plotly

--- components/outputs/plot-plotly/app-express.py
+++ components/outputs/plot-plotly/app-express.py
@@ -1,26 +1,23 @@
-# FIXME: Rewrite as an Express app
 import plotly.express as px
 from palmerpenguins import load_penguins
-from shiny import App, reactive, ui
-from shinywidgets import output_widget, render_widget  #<<
+from shiny.express import input, ui
+from shinywidgets import render_widget  # <<
 
-app_ui = ui.page_fluid(
-    ui.input_slider("n", "Number of bins", 1, 100, 20),
-    output_widget("plot"),  #<<
-)
+penguins = load_penguins()
 
-def server(input, output, session):
-    @render_widget  #<<
-    def plot(): #<<
-        penguins = load_penguins()
-        scatterplot = px.histogram(
-            data_frame=penguins, x="body_mass_g", nbins=input.n()
-        ).update_layout(
-            title={"text": "Penguin Mass", "x": 0.5},
-            yaxis_title="Count",
-            xaxis_title="Body Mass (g)",
-        )
+ui.input_slider("n", "Number of bins", 1, 100, 20)
 
-        return scatterplot  #<<
 
-app = App(app_ui, server)
+@render_widget  # <<
+def plot():  # <<
+    scatterplot = px.histogram(
+        data_frame=penguins,
+        x="body_mass_g",
+        nbins=input.n(),
+    ).update_layout(
+        title={"text": "Penguin Mass", "x": 0.5},
+        yaxis_title="Count",
+        xaxis_title="Body Mass (g)",
+    )
+
+    return scatterplot  # <<
