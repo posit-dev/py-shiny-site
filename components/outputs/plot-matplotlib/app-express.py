7e3126bf119718623eb3e43617083982190ba1ad
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-11 15:22:57 -0500

    expressify components/outputs/plot-matplotlib

--- components/outputs/plot-matplotlib/app-express.py
+++ components/outputs/plot-matplotlib/app-express.py
@@ -1,25 +1,20 @@
-# FIXME: Rewrite as an Express app
 import matplotlib.pyplot as plt
 from palmerpenguins import load_penguins
-from shiny import App, render, ui
+from shiny import render
+from shiny.express import input, ui
 
-app_ui = ui.page_fluid(
-    ui.input_slider("n", "Number of bins", 0, 100, 20),
-    ui.output_plot("plot"),  #<<
-)
+ui.input_slider("n", "Number of bins", 0, 100, 20)
 
-def server(input, output, session):
-    @render.plot(alt="A histogram")  #<<
-    def plot():  #<<
-        df = load_penguins()
-        mass = df["body_mass_g"]
 
-        fig, ax = plt.subplots()
-        ax.hist(mass, input.n(), density=True)
-        ax.set_title("Palmer Penguin Masses")
-        ax.set_xlabel("Mass (g)")
-        ax.set_ylabel("Density")
+@render.plot(alt="A histogram")  # <<
+def plot():  # <<
+    df = load_penguins()
+    mass = df["body_mass_g"]
 
-        return fig  #<<
+    fig, ax = plt.subplots()
+    ax.hist(mass, input.n(), density=True)
+    ax.set_title("Palmer Penguin Masses")
+    ax.set_xlabel("Mass (g)")
+    ax.set_ylabel("Density")
 
-app = App(app_ui, server, debug=True)
+    return fig  # <<
--- components/outputs/plot-matplotlib/app-variation-plot-as-input-express.py
+++ components/outputs/plot-matplotlib/app-variation-plot-as-input-express.py
@@ -1,27 +1,30 @@
-# FIXME: Rewrite as an Express app
 import matplotlib.pyplot as plt
 from palmerpenguins import load_penguins
-from shiny import App, render, ui
+from shiny import render
+from shiny.express import input, suspend_display, ui
 
-app_ui = ui.page_fluid(
-    ui.output_plot(
-        "plot", 
-        click=True, #<<
-        dblclick=True, #<<
-        hover=True, #<<
-        brush=True #<<
-    ),
-    "Click:",
-    ui.output_text_verbatim("clk", placeholder=True),
-    "Double Click:",
-    ui.output_text_verbatim("dblclk", placeholder=True),
-    "Hover:",
-    ui.output_text_verbatim("hvr", placeholder=True),
-    "Brush",
-    ui.output_text_verbatim("brsh", placeholder=True),
+ui.output_plot(
+    "plot",
+    click=True,  # <<
+    dblclick=True,  # <<
+    hover=True,  # <<
+    brush=True,  # <<
 )
 
-def server(input, output, session):
+"Click:"
+ui.output_text_verbatim("clk", placeholder=True)
+"Double Click:"
+ui.output_text_verbatim("dblclk", placeholder=True)
+"Hover:"
+ui.output_text_verbatim("hvr", placeholder=True)
+"Brush"
+ui.output_text_verbatim("brsh", placeholder=True)
+
+
+with suspend_display():
+    # Note that this Express app uses `suspend_display()` so that we can
+    # manually add the `ui.output_plot()` and others to the page.
+
     @render.plot(alt="A histogram")
     def plot():
         df = load_penguins()
@@ -48,5 +51,3 @@ def server(input, output, session):
     @render.text
     def brsh():
         return input.plot_brush()
-        
-app = App(app_ui, server, debug=True)
