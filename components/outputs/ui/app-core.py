9b1ceb534407333a6da0617115008b1e3358029a
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-12 15:29:56 -0500

    use `@render.display`

--- components/outputs/ui/app-core.py
+++ components/outputs/ui/app-core.py
@@ -2,14 +2,14 @@ from shiny import App, reactive, render, ui
 
 app_ui = ui.page_fluid(
     ui.input_switch("show_slider", "Show slider", True),
-    ui.output_ui("uiElement"),  # <<
+    ui.output_ui("ui_slider"),  # <<
 )
 
 
 def server(input, output, session):
     @render.ui  # <<
     @reactive.event(input.show_slider)  # <<
-    def uiElement():  # <<
+    def ui_slider():  # <<
         if input.show_slider():
             value = input.slider() if "slider" in input else 5
             return ui.input_slider(
--- components/outputs/ui/app-express.py
+++ components/outputs/ui/app-express.py
@@ -4,8 +4,8 @@ from shiny.express import input, ui
 ui.input_switch("show_slider", "Show slider", True)
 
 
-@render.ui  # <<
-def uiElement():  # <<
+@render.display  # <<
+def ui_slider():  # <<
     if input.show_slider():
         value = input.slider() if "slider" in input else 5
-        return ui.input_slider("slider", "Choose a number", min=1, max=10, value=value)
+        ui.input_slider("slider", "Choose a number", min=1, max=10, value=value)
