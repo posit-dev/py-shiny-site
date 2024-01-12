0a6449872ea98653712b14631e15f7442f0cf252
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-11 22:32:19 -0500

    expressify components/outputs/value-box
    Also add `resources` to apps that have additional files for shinylive

--- components/outputs/value-box/app-variation-reactive-value-box-core.py
+++ components/outputs/value-box/app-variation-reactive-value-box-core.py
@@ -1,20 +1,34 @@
 import faicons
-from shiny import App, Inputs, Outputs, Session, render, ui
+from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n", "Number of Billions", 1, 100, 20),  #<<
-    ui.value_box(
-        title="KPI Title",
-        showcase=faicons.icon_svg('piggy-bank', width="50px"),
-        value=ui.output_text("billions"),  #<<
-        theme="bg-gradient-indigo-purple",
-        full_screen=True,
+    ui.layout_columns(
+        ui.h2("Spend Jeff's 2023 Earnings"),
+        ui.input_slider("pct", "Percent of $70 Billion to donate", 0, 100, 20),  # <<
+        ui.value_box(
+            title="Save",
+            showcase=faicons.icon_svg("piggy-bank", width="50px"),
+            value=ui.output_ui("save"),  # <<
+            theme="bg-gradient-orange-red",
+        ),
+        ui.value_box(
+            title="Donate",
+            showcase=faicons.icon_svg("hand-holding-dollar", width="50px"),
+            value=ui.output_ui("donate"),  # <<
+            theme="bg-gradient-blue-purple",
+        ),
     ),
 )
 
+
 def server(input, output, session):
-    @render.text  #<<
-    def billions():  #<<
-        return f"${input.n()} Billion Dollars"  #<<
+    @render.text  # <<
+    def save():  # <<
+        return f"${(1 - input.pct() / 100) * 70:.1f} Billion"  # <<
+
+    @render.text  # <<
+    def donate():  # <<
+        return f"${input.pct() / 100 * 70:.1f} Billion"  # <<
+
 
 app = App(app_ui, server)
--- components/outputs/value-box/app-variation-sparklines-core.py
+++ components/outputs/value-box/app-variation-sparklines-core.py
@@ -1,4 +1,4 @@
-# see shiny-live page for details on additional files
+# Open in shinylive to see additional files
 
 from pathlib import Path
 
@@ -9,18 +9,17 @@ from shiny import App, ui
 
 appdir = Path(__file__).parent
 
-app_ui = ui.page_fillable(
+app_ui = ui.page_fixed(
     ui.include_css(appdir / "styles.css"),
     ui.value_box(
         "Total Sales in Q2",
         "$2.45M",
-        {"class": "shadow-none"},
         showcase=sw.output_widget("sparkline"),
-        showcase_layout=ui.showcase_left_center(width="40%"),
+        showcase_layout="bottom",
     ),
-    padding=0,
     fillable_mobile=True,
 )
+
 
 def server(input, output, session):
     @sw.render_widget
@@ -37,12 +36,13 @@ def server(input, output, session):
         fig.update_xaxes(visible=False, showgrid=False)
         fig.update_yaxes(visible=False, showgrid=False)
         fig.update_layout(
-            height=60,
+            height=100,
             hovermode="x",
             margin=dict(t=0, r=0, l=0, b=0),
             plot_bgcolor="rgba(0,0,0,0)",
             paper_bgcolor="rgba(0,0,0,0)",
         )
         return fig
+
 
 app = App(app_ui, server)
