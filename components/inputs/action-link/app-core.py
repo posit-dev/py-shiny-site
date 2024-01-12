27edfce7eaaecad1a90ffb00a16bb8abdcd3ea14
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-12 11:00:54 -0500

    tweak action-link preview app

--- components/inputs/action-link/app-core.py
+++ components/inputs/action-link/app-core.py
@@ -1,14 +1,14 @@
 from shiny import App, reactive, render, ui
 
 app_ui = ui.page_fluid(
-    ui.input_action_link("action_link", "Action"),  # <<
+    ui.input_action_link("action_link", "Increase Number"),  # <<
     ui.output_text("counter"),
 )
 
 
 def server(input, output, session):
     @render.text()
-    @reactive.event(input.action_link)
+    @reactive.event(input.action_link)  # <<
     def counter():
         return f"{input.action_link()}"
 
--- components/inputs/action-link/app-express.py
+++ components/inputs/action-link/app-express.py
@@ -1,11 +1,10 @@
 from shiny import reactive, render
 from shiny.express import input, ui
 
-
-ui.input_action_link("action_link", "Action")  # <<
+ui.input_action_link("action_link", "Increase Number")  # <<
 
 
 @render.text()
-@reactive.event(input.action_link)
+@reactive.event(input.action_link)  # <<
 def counter():
     return f"{input.action_link()}"
