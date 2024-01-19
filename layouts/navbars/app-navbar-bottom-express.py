b2619d6f070e46f1fdd3bc2ae8eef36fe632027b
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-19 17:14:12 -0500

    expressify layout/navbars

---
+++ layouts/navbars/app-navbar-bottom-express.py
@@ -0,0 +1,21 @@
+from shiny.express import ui
+from shiny.ui import page_navbar
+
+ui.page_opts(
+    title="App with navbar",  # <<
+    page_fn=lambda *args, **kwargs: page_navbar(
+        *args,
+        id="page",
+        position="fixed-bottom",  # <<
+        **kwargs,
+    ),  # <<
+)
+
+with ui.nav_panel("A"):  # <<
+    "Page A content"
+
+with ui.nav_panel("B"):  # <<
+    "Page B content"
+
+with ui.nav_panel("C"):  # <<
+    "Page C content"
