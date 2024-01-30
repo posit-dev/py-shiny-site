acadbed37107a69c154168cb5905679ed62a64f9
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-29 20:57:34 -0500

    chore(layout/panels): Add image back to floating panel
    But make focus on the floating panel

--- layouts/panels-cards/app-panel-absolute-core.py
+++ layouts/panels-cards/app-panel-absolute-core.py
@@ -1,6 +1,7 @@
+from htmltools import css
 from shiny import App, ui
 
-app_ui = ui.page_fluid(
+app_ui = ui.page_fillable(
     ui.panel_absolute(  # <<
         ui.panel_well(
             ui.panel_title("Draggable panel"),
@@ -11,6 +12,12 @@ app_ui = ui.page_fluid(
         top="50px",  # <<
         draggable=True,  # <<
     ),  # <<
+    style=css(
+        background_image="url(https://unsplash.com/photos/XKXGghL7GQc/download?force=true&w=1920)",
+        background_repeat="no-repeat",
+        background_size="cover",
+        background_position="center bottom",
+    ),
 )
 
 
