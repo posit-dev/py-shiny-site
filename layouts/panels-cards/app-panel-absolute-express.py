fe288e4118db0681f1ae6e00dfb9036589bb33be
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-30 09:41:01 -0500

    Chore/component-gallery-touch-ups (#92)
    * chore(components): fix link to render.express
    
    * remove duplicated rules
    
    * remove double border in app-preview shinylive apps
    
    * chore(layouts/tabs): Fix padding in app previews
    
    * chore(layout/panels): Add image back to floating panel
    
    But make focus on the floating panel
    
    * make unhlighted lines less invisible

--- layouts/panels-cards/app-panel-absolute-express.py
+++ layouts/panels-cards/app-panel-absolute-express.py
@@ -1,11 +1,24 @@
-from shiny.express import ui
+from functools import partial
 
-with ui.panel_absolute(
+from htmltools import css
+from shiny.express import ui
+from shiny.ui import page_fillable
+
+page_style = css(
+    background_image="url(https://unsplash.com/photos/XKXGghL7GQc/download?force=true&w=1920)",
+    background_repeat="no-repeat",
+    background_size="cover",
+    background_position="center bottom",
+)
+
+ui.page_opts(page_fn=partial(page_fillable, style=page_style))
+
+with ui.panel_absolute(  # <<
     width="300px",  # <<
     right="50px",  # <<
     top="50px",  # <<
     draggable=True,  # <<
 ):  # <<
-    with ui.panel_well():  # <<
-        ui.h2("Draggable panel")  # <<
-        "Move this panel anywhere you want."  # <<
+    with ui.panel_well():
+        ui.h2("Draggable panel")
+        "Move this panel anywhere you want."
