455fe68d535c86a658c0d9a2488141d9545bd11b
gregswinehart <greg@posit.co>
2024-04-11 15:27:58 -0700

    text-center

--- components/inputs/dark-mode/app-detail-preview.py
+++ components/inputs/dark-mode/app-detail-preview.py
@@ -1,7 +1,7 @@
 ## file: app.py
 from shiny import ui, render, App
 
-app_ui = ui.page_fluid(ui.input_dark_mode()).add_class("h-100 w-100 align-content-center")
+app_ui = ui.page_fluid(ui.input_dark_mode()).add_class("h-100 w-100 align-content-center text-center")
 
 def server(input, output, session):
     pass
