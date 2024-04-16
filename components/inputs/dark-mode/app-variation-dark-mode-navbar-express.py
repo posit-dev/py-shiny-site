9f963eac517632b9ab727266a3dd2a878d6261e2
skaltman <sara.altman@posit.co>
2024-04-16 11:22:37 -0700

    add variation

---
+++ components/inputs/dark-mode/app-variation-dark-mode-navbar-express.py
@@ -0,0 +1,12 @@
+from shiny import ui, render, App
+
+app_ui = ui.page_navbar( #<<
+    ui.nav_spacer(), #<<
+    ui.nav_control(ui.input_dark_mode()), #<<
+    title="Dark mode switch in navbar" #<<
+) #<<
+
+def server(input, output, session):
+    pass
+
+app = App(app_ui, server)
