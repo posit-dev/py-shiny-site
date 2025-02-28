04439a0983a1fe5c5359204157ea2dd3e59f1064
Carson <cpsievert1@gmail.com>
2025-02-28 13:53:38 -0600

    wip write about newer chat additions

--- components/display-messages/chat/app-express.py
+++ components/display-messages/chat/app-express.py
@@ -6,13 +6,13 @@ ui.page_opts(
     fillable_mobile=True,
 )
 
-# Create a chat instance and display it
+# Create a chat instance and display it # <<
 chat = ui.Chat(id="chat")  # <<
 chat.ui()  # <<
 
 
-# Define a callback to run when the user submits a message
+# Define a callback to run when the user submits a message # <<
 @chat.on_user_submit  # <<
 async def handle_user_input(user_input: str):  # <<
-    # Simply echo the user's input back to them
+    # Simply echo the user's input back to them # <<
     await chat.append_message(f"You said: {user_input}")  # <<
