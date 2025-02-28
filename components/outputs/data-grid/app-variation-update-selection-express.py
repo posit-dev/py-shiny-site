ef51158fc399310f43436045dc6b35598ed273f9
Sara Altman <sarakaialtman@gmail.com>
2025-02-28 12:31:20 -0800

    Add `.update_cell_selection()` variation to DataGrid and DataTable components (#219)
    * add update cell selection variation
    
    * fix function name
    
    * update example
    
    * add data table variation
    
    ---------
    
    Co-authored-by: Carson Sievert <cpsievert1@gmail.com>

---
+++ components/outputs/data-grid/app-variation-update-selection-express.py
@@ -0,0 +1,30 @@
+from palmerpenguins import load_penguins
+from shiny import reactive
+from shiny.express import input, render, ui
+
+penguins = load_penguins()
+
+ui.h2("Palmer Penguins")
+
+ui.input_action_button("update_selection", "Update row selection")
+ui.input_action_button("reset_selection", "Reset row selection")
+
+ui.h5("Current selection: ", {"class": "pt-2"})
+
+@render.code
+def _():  # <<
+    return penguins_df.cell_selection()["rows"]  # <<
+
+@render.data_frame
+def penguins_df():  # <<
+    return render.DataGrid(penguins, selection_mode="rows")  # <<
+
+@reactive.effect 
+@reactive.event(input.update_selection)
+async def _():  # <<
+    await penguins_df.update_cell_selection({"type": "row", "rows": [1, 2, 8]})  # <<
+
+@reactive.effect
+@reactive.event(input.reset_selection)
+async def _():  # <<
+    await penguins_df.update_cell_selection({"type": "row", "rows": []})   # <<
