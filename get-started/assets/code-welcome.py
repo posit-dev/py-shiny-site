4ec63de00082be0953139652163d11d0843e4b64
Daniel Chen <chendaniely@users.noreply.github.com>
2025-04-29 15:21:13 -0700

    update shinylive app to highlight species hist

--- get-started/assets/code-welcome.py
+++ get-started/assets/code-welcome.py
@@ -1,10 +1,18 @@
-import seaborn as sns
+from palmerpenguins import load_penguins
+from plotnine import aes, geom_histogram, ggplot, theme_minimal
 from shiny.express import input, render, ui
 
-df = sns.load_dataset("penguins")
+all = load_penguins()
+species = all["species"].unique().tolist()
 
-ui.input_slider("bins", "Number of Bins", min=5, max=20, value=15, step=1)
+ui.input_radio_buttons("species", "Species", species, inline=True)
 
 @render.plot(height=300)
 def plot():
-    sns.histplot(data=df, x="body_mass_g", bins=input.bins())
+    sel = all[all["species"] == input.species()]
+    return (
+        ggplot()
+        + geom_histogram(all, aes(x="bill_length_mm"), fill="#C2C2C4", binwidth=1)
+        + geom_histogram(sel, aes(x="bill_length_mm"), fill="#447099", binwidth=1)
+        + theme_minimal()
+    )
