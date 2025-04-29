f5d9362ccc923f802dbea71be6fb9fc4bcc8b765
Daniel Chen <chendaniely@users.noreply.github.com>
2025-04-29 15:51:14 -0700

    move aes to ggplot. rename df pop to dat

--- get-started/assets/code-welcome.py
+++ get-started/assets/code-welcome.py
@@ -2,17 +2,18 @@ from palmerpenguins import load_penguins
 from plotnine import aes, geom_histogram, ggplot, theme_minimal
 from shiny.express import input, render, ui
 
-all = load_penguins()
-species = all["species"].unique().tolist()
+dat = load_penguins()
+species = dat["species"].unique().tolist()
 
 ui.input_radio_buttons("species", "Species", species, inline=True)
 
+
 @render.plot(height=300)
 def plot():
-    sel = all[all["species"] == input.species()]
+    sel = dat[dat["species"] == input.species()]
     return (
-        ggplot()
-        + geom_histogram(all, aes(x="bill_length_mm"), fill="#C2C2C4", binwidth=1)
-        + geom_histogram(sel, aes(x="bill_length_mm"), fill="#447099", binwidth=1)
+        ggplot(aes(x="bill_length_mm"))
+        + geom_histogram(dat, fill="#C2C2C4", binwidth=1)
+        + geom_histogram(sel, fill="#447099", binwidth=1)
         + theme_minimal()
     )
