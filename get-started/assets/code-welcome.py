04681d7230ded81c404b33b3cb496838fd86ce11
Daniel Chen <chendaniely@gmail.com>
2025-05-30 06:33:46 -0600

    chore: Improve get started app example (#286)
    * remove old carbon svg code files
    
    * update shinylive app to highlight species hist
    
    * move aes to ggplot. rename df pop to dat
    
    * chore: adjust position of floating-code window
    
    ---------
    
    Co-authored-by: Daniel Chen <chendaniely@users.noreply.github.com>
    Co-authored-by: Garrick Aden-Buie <garrick@adenbuie.com>

--- get-started/assets/code-welcome.py
+++ get-started/assets/code-welcome.py
@@ -1,10 +1,19 @@
-import seaborn as sns
+from palmerpenguins import load_penguins
+from plotnine import aes, geom_histogram, ggplot, theme_minimal
 from shiny.express import input, render, ui
 
-df = sns.load_dataset("penguins")
+dat = load_penguins()
+species = dat["species"].unique().tolist()
 
-ui.input_slider("bins", "Number of Bins", min=5, max=20, value=15, step=1)
+ui.input_radio_buttons("species", "Species", species, inline=True)
+
 
 @render.plot(height=300)
 def plot():
-    sns.histplot(data=df, x="body_mass_g", bins=input.bins())
+    sel = dat[dat["species"] == input.species()]
+    return (
+        ggplot(aes(x="bill_length_mm"))
+        + geom_histogram(dat, fill="#C2C2C4", binwidth=1)
+        + geom_histogram(sel, fill="#447099", binwidth=1)
+        + theme_minimal()
+    )
