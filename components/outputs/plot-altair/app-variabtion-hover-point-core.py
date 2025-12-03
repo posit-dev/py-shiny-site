1a4631fda721f44922099c1cb70e3e2e7f795509
Daniel Chen <chendaniely@users.noreply.github.com>
2025-12-02 22:24:00 -0800

    altair hover core example

---
+++ components/outputs/plot-altair/app-variabtion-hover-point-core.py
@@ -0,0 +1,52 @@
+import altair as alt
+import pandas as pd
+from shiny import App, render, ui
+from shinywidgets import output_widget, reactive_read, render_altair
+from vega_datasets import data
+
+df = data.cars()
+
+app_ui = ui.page_fillable(
+    "Hover over a point to see its info",
+    ui.layout_column_wrap(
+        output_widget("jchart"),
+        ui.output_data_frame("hover_info"),
+    ),
+)
+
+
+def server(input, output, session):
+    @render_altair
+    def jchart():
+        hover = alt.selection_point(
+            name="hover",
+            on="mouseover",
+            empty=False,
+        )
+        return (
+            alt.Chart(df)
+            .mark_circle(size=60)
+            .encode(
+                x="Horsepower:Q",
+                y="Miles_per_Gallon:Q",
+                color="Origin:N",
+                opacity=alt.condition(hover, alt.value(1), alt.value(0.1)),
+                size=alt.condition(hover, alt.value(250), alt.value(50)),
+            )
+            .add_params(hover)
+        )
+
+    @render.data_frame
+    def hover_info():
+        pt = reactive_read(jchart.widget.selections, "hover")
+        hover_idx = pt.value
+        selected = df.iloc[hover_idx]
+        if hover_idx:
+            transposed = selected.T.reset_index()
+            transposed.columns = ["Variable", "Value"]
+            return transposed
+        else:
+            return pd.DataFrame({"Variable": df.columns, "Value": ""})
+
+
+app = App(app_ui, server)
