d414d51240aa8a364e1cf94a9d8caffaefb7eec1
Daniel Chen <chendaniely@users.noreply.github.com>
2025-12-02 22:10:44 -0800

    show a transposed dataframe

--- components/outputs/plot-altair/app-variation-select-point-express.py
+++ components/outputs/plot-altair/app-variation-select-point-express.py
@@ -1,34 +1,44 @@
 import altair as alt
-from shiny.express import render
-from shinywidgets import reactive_read, render_altair, render_widget  # <<
+import pandas as pd
+from shiny.express import render, ui
+from shinywidgets import reactive_read, render_altair  # <<
 from vega_datasets import data
 
-source = data.cars()
+df = data.cars()
 
 "Hover over a point to see its info"
 
+with ui.layout_column_wrap():
 
-@render_widget
-def jchart():
-    hover = alt.selection_point(
-        name="hover", on="mouseover", empty=False
-    )  # <<
-    return (
-        alt.Chart(source)
-        .mark_circle(size=60)
-        .encode(
-            x="Horsepower:Q",
-            y="Miles_per_Gallon:Q",
-            color="Origin:N",
-            opacity=alt.condition(hover, alt.value(1), alt.value(0.1)),
-            size=alt.condition(hover, alt.value(250), alt.value(50)),
+    @render_altair
+    def jchart():
+        hover = alt.selection_point(  # <<
+            name="hover",  # <<
+            on="mouseover",  # <<
+            empty=False,  # <<
+        )  # <<
+        return (
+            alt.Chart(df)
+            .mark_circle(size=60)
+            .encode(
+                x="Horsepower:Q",
+                y="Miles_per_Gallon:Q",
+                color="Origin:N",
+                opacity=alt.condition(hover, alt.value(1), alt.value(0.1)),
+                size=alt.condition(hover, alt.value(250), alt.value(50)),
+            )
+            .add_params(hover)
         )
-        .add_params(hover)
-    )
 
-
-@render.data_frame
-def hover_info():
-    pt = reactive_read(jchart.widget.selections, "hover")  # <<
-    indices = pt.value
-    return source.iloc[indices]
+    @render.data_frame
+    def hover_info():
+        # pt is a JupyterChart IndexSelection object
+        pt = reactive_read(jchart.widget.selections, "hover")  # <<
+        hover_idx = pt.value
+        selected = df.iloc[hover_idx]
+        if hover_idx:
+            transposed = selected.T.reset_index()
+            transposed.columns = ["Variable", "Value"]
+            return transposed
+        else:
+            return pd.DataFrame({"Variable": df.columns, "Value": ""})
