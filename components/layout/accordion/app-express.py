eb3a6b1bba5acc3b4960d2b2aa2f8138da58c29c
Barret Schloerke <barret@posit.co>
2026-07-23 10:19:31 -0400

    Add Accordion layout component documentation page

---
+++ components/layout/accordion/app-express.py
@@ -0,0 +1,14 @@
+from shiny.express import input, render, ui
+
+with ui.accordion(id="acc", open=["Section A"]):  # <<
+    with ui.accordion_panel("Section A"):
+        "Some narrative for section A"
+    with ui.accordion_panel("Section B"):
+        "Some narrative for section B"
+    with ui.accordion_panel("Section C"):
+        "Some narrative for section C"
+
+
+@render.text
+def selected():
+    return f"Open panel(s): {input.acc()}"
